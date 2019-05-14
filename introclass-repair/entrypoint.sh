d14a68b2face2a37add6bda3cd6e348a99ee7197
zhenyudg <8439020+zhenyudg@users.noreply.github.com>
2019-05-14 19:18:32 -0400

    Update entrypoint.sh

--- introclass-repair/entrypoint.sh
+++ introclass-repair/entrypoint.sh
@@ -9,6 +9,11 @@ git clone --single-branch --branch Yiwei-Lyu-mini-proj https://github.com/square
 export GP4J_HOME=$(pwd)
 
 cd
+git clone --single-branch --branch patch-diversity-evaluation https://github.com/squaresLab/genprog4java.git gp4j-patch-div-eval \
+&& cd gp4j-patch-div-eval \
+&& mvn package
+
+cd
 git clone https://github.com/squaresLab/GenProgInvDiv-IntroClassJava-Scripts.git IntroClassScripts
 #&& cd IntroClassScripts
 #&& bash preprocessIntroClassJava.sh ${ICJ_HOME}
